a4640a9d9e845a443fed53e1fc0f088b362c4163
Altay Batuhan <45828884+altarrok@users.noreply.github.com>
2020-08-04 14:09:19 -0700

    Adding bootstrap beer data

---
+++ src/main/java/spring/altarok/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,47 @@
+package spring.altarok.msscbeerservice.bootstrap;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import spring.altarok.msscbeerservice.domain.Beer;
+import spring.altarok.msscbeerservice.repositories.BeerRepository;
+
+import java.math.BigDecimal;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (beerRepository.count() == 0) {
+            loadBeerObjects();
+        }
+    }
+
+    private void loadBeerObjects() {
+            beerRepository.save(Beer.builder()
+                    .beerName("McBeer")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(24)
+                    .upc(18200007712L)
+                    .price(new BigDecimal("1.99"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("BeerHut")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(10)
+                    .minOnHand(4)
+                    .upc(18200127712L)
+                    .price(new BigDecimal("13.99"))
+                    .build());
+
+            log.info("Loaded Beers: " + beerRepository.count());
+    }
+}
--- src/main/java/spring/altarok/msscbeerservice/repositories/BeerRepository.java
+++ src/main/java/spring/altarok/msscbeerservice/repositories/BeerRepository.java
@@ -1,10 +1,9 @@
 package spring.altarok.msscbeerservice.repositories;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import spring.altarok.msscbeerservice.domain.Beer;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
-    
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
 }
